dfe9334bbeb5c5f87ff65afc49c205310694cbf7
Michal Pochopien <muniek@muniek.pl>
2012-09-13 12:51:02 +0200

    Dodanie klasy skrzyżowania

---
+++ NSJC_TD/NSJC_TD/Junction.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NSJC_TD
+{
+    class Junction:GameObject
+    {
+        public override void Update()
+        { 
+        }
+    }
+}
